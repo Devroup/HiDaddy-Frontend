48143aec5a27ca725cf5d8bbe25f3e131c7e5672
mcy0325 <mcy325@naver.com>
2025-06-24 03:39:00 +0900

    add: project files

--- HiDaddy/src/screens/diary/DiaryScreen.js
+++ HiDaddy/src/screens/diary/DiaryScreen.js
@@ -1,1 +1,23 @@
 import React from 'react';
+import styled from 'styled-components/native';
+import colors from '../../constants/colors';
+
+const DiaryScreen = () => (
+  <Container>
+    <Title>Diary Screen</Title>
+  </Container>
+);
+
+export default DiaryScreen;
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: ${colors.black};
+`;
--- HiDaddy/src/screens/home/HomeScreen.js
+++ HiDaddy/src/screens/home/HomeScreen.js
@@ -1,1 +1,23 @@
 import React from 'react';
+import styled from 'styled-components/native';
+import colors from '../../constants/colors';
+
+const HomeScreen = () => (
+  <Container>
+    <Title>Home Screen</Title>
+  </Container>
+);
+
+export default HomeScreen;
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: ${colors.black};
+`;
